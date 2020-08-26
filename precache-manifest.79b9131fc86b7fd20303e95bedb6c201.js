03163381b18ef9fbca01648e3cdd4791bbfcbe1f
Deepak <deepak.negi343@gmail.com>
2020-08-26 14:18:22 +0530

    Updates

---
+++ precache-manifest.79b9131fc86b7fd20303e95bedb6c201.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f42a9da96a141e0362aab0db22843e",
+    "url": "/shopping_cart_react/index.html"
+  },
+  {
+    "revision": "b6659a6e605d2654101c",
+    "url": "/shopping_cart_react/static/css/main.0f35e5ad.chunk.css"
+  },
+  {
+    "revision": "53346d999532ba572cc2",
+    "url": "/shopping_cart_react/static/js/2.f07c1f7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping_cart_react/static/js/2.f07c1f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6659a6e605d2654101c",
+    "url": "/shopping_cart_react/static/js/main.22d3bdc1.chunk.js"
+  },
+  {
+    "revision": "fbd32c235a5d7ca78cda",
+    "url": "/shopping_cart_react/static/js/runtime-main.35b57e68.js"
+  },
+  {
+    "revision": "3c5c75a84ce80fa8f964ed15b841b5ae",
+    "url": "/shopping_cart_react/static/media/logo.3c5c75a8.PNG"
+  }
+]);
